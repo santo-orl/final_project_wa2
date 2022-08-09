ca83ad79d88d6df9b6726a2c0a44745d18b1edda
santo-orl <orlandosantino11@gmail.com>
2022-08-09 10:15:13 +0200

    la zingara mi sta cercando

--- traveler_service/build.gradle.kts
+++ traveler_service/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation ("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    implementation("junit:junit:4.13.1")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
     runtimeOnly ("org.postgresql:postgresql")
---
+++ traveler_service/src/test/kotlin/it/polito/traveler_service/unitTests/UserDetailsUnitTests.kt
@@ -0,0 +1,78 @@
+package it.polito.traveler_service.unitTests
+
+import it.polito.traveler_service.dtos.TicketPurchasedDTO
+import it.polito.traveler_service.repositories.TicketPurchasedRepository
+import it.polito.traveler_service.services.TicketPurchasedService
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.test.context.junit4.SpringRunner
+
+class UserDetailsUnitTests {
+
+    @RunWith(SpringRunner::class)
+    @ExtendWith(MockitoExtension::class)
+    class TicketPurchasedTest{
+
+        lateinit var ticketPurchasedService : TicketPurchasedService
+        lateinit var ticketPurchasedRepository: TicketPurchasedRepository
+        lateinit var ticketPurchasedDTO: TicketPurchasedDTO
+
+        init{
+            ticketPurchasedService = Mockito.mock(TicketPurchasedService::class.java)
+            ticketPurchasedRepository = Mockito.mock(TicketPurchasedRepository::class.java)
+            ticketPurchasedDTO = Mockito.mock(TicketPurchasedDTO::class.java)
+        }
+
+
+        @Test
+        //suppone sia presente il tipo di ticket che voglio controllare
+        fun checkGetsAllTickets(){
+            //suppongo id 0 sia presente
+            var ret = ticketPurchasedService.getAllTickets(0)
+            assert(ret.isNotEmpty())
+        }
+
+        @Test
+        //suppone non sia presente il tipo di ticket che sto cercando
+        fun checkGetsAllTicketsNone(){
+            //suppongo id 0 non sia presente
+            var ret = ticketPurchasedService.getAllTickets(0)
+            assert(ret.isEmpty())
+        }
+
+        @Test
+        fun checkZoneInvalidCreateTicket(){
+            Assertions.assertThrows(Exception::class.java){
+                ticketPurchasedService.createTicket("",0,"2022-08-09 10:08","DAILY")
+            }
+        }
+
+        @Test
+        fun checkIdInvalidCreateTicket(){
+            //suppongo id 100 non presente
+            Assertions.assertThrows(Exception::class.java){
+                ticketPurchasedService.createTicket("1",100,"2022-08-09 10:08","DAILY")
+            }
+        }
+
+        @Test
+        fun checkValidFromInvalidCreateTicket(){
+            Assertions.assertThrows(Exception::class.java){
+                ticketPurchasedService.createTicket("1",1,"2042-08-09 10:08","DAILY")
+            }
+        }
+
+        @Test
+        fun checkTypeInvalidCreateTicket(){
+            Assertions.assertThrows(Exception::class.java){
+                ticketPurchasedService.createTicket("1",1,"2022-08-09 10:08","")
+            }
+        }
+
+
+    }
+}
