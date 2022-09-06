13d9c1cf8f24ea81a07520da27ab8c69ca6c830b
antmat99 <antmat99@gmail.com>
2022-09-06 11:45:57 +0200

    login_service: fatta security con coroutine (2)

--- login_service/src/main/kotlin/it/polito/login_service/security/SecurityConfiguration.kt
+++ login_service/src/main/kotlin/it/polito/login_service/security/SecurityConfiguration.kt
@@ -2,33 +2,43 @@ package it.polito.login_service.security
 
 
 import it.polito.login_service.entities.Role
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
+import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.server.SecurityWebFilterChain
 
 
 @Configuration
-class SecurityConfiguration(val bCryptPasswordEncoder: BCryptPasswordEncoder) : WebSecurityConfigurerAdapter() {
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+class SecurityConfiguration {
 
-    @Bean
-    fun authenticationJwtTokenFilter(): JwtAuthenticationTokenFilter? {
-        return JwtAuthenticationTokenFilter()
-    }
+    @Autowired
+    lateinit var filter: JwtAuthenticationTokenFilter
 
     //qui gestisco la protezione degli url
-    override fun configure(http: HttpSecurity) {
-        //super.configure(http)
-        http
+
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        return http
             .csrf().disable()
-            .antMatcher("/admin/**")
-            .authorizeRequests()
-            .anyRequest().hasAnyAuthority(Role.ADMIN.toString())
-            .and()
-            .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterAt(filter, SecurityWebFiltersOrder.AUTHORIZATION)
+            .formLogin().disable()
+            .httpBasic().disable()
+            .authorizeExchange()
+            .pathMatchers("/admin/**").hasAuthority("ADMIN")
+            .anyExchange().permitAll()
+            .and().build()
     }
 
 }
